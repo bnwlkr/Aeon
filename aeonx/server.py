422d2c27ee90e25c15ee0435f2e3ce21374d6afd
Ben Walker <bnwlkr@icloud.com>
2019-01-27 00:29:24 +0000

    server now working

--- aeonx/server.py
+++ aeonx/server.py
@@ -7,17 +7,17 @@ import os
 async def boot(websocket, path):
     print("connected client")
     try:
-        await handleClient(client)
+        await handleClient(websocket)
     except websockets.exceptions.ConnectionClosed:
         print("connection closed")
 
 
-async def handleClient(client):
-    async for message in client.websocket:
+async def handleClient(websocket):
+    async for message in websocket:
         print(message)
     
 
-start_server = websockets.serve(boot, port=8000)
+start_server = websockets.serve(boot, '10.0.0.8', port=8000)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
